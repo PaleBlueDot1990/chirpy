6b065aa808ebfbe414e10b8ce992448c5b379f80
Bhuvnesh <bhuvnesh.bhuwan.golden@gmail.com>
2025-06-05 17:27:07 +0530

    added logic to find and clean profane chirps

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
-	"strconv"
+	"strings"
 	"sync/atomic"
 )
 
 type apiConfig struct{
 	fileServerHits atomic.Int32
+}
+
+type chirp struct {
+	Body string `json:"body"`
 }
 
 func main() {
@@ -17,8 +22,9 @@ func main() {
 
 	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
 	mux.HandleFunc("GET /api/healthz", healthzHandler)
-	mux.HandleFunc("GET /api/metrics", cfg.getMetricsHandler)
-	mux.HandleFunc("POST /api/reset", cfg.resetMetricsHandler)
+	mux.HandleFunc("GET /admin/metrics", cfg.getMetricsHandler)
+	mux.HandleFunc("POST /admin/reset", cfg.resetMetricsHandler)
+	mux.HandleFunc("POST /api/validate_chirp", validateChirp)
 
 	server := http.Server{
 		Addr: ":8080",
@@ -49,8 +55,9 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 //metrics handler: returns the current counter
 func (cfg *apiConfig) getMetricsHandler(w http.ResponseWriter, req *http.Request) {
 	count := cfg.fileServerHits.Load()
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.Write([]byte("Hits: " + strconv.Itoa(int(count))))
+	w.Header().Set("Content-Type", "text/html")
+	html := fmt.Sprintf("<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>", count)
+	w.Write([]byte(html))
 }
 
 //reset handler: resets the counter 
@@ -58,4 +65,50 @@ func (cfg *apiConfig) resetMetricsHandler(w http.ResponseWriter, req *http.Reque
 	cfg.fileServerHits.Store(0)
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.Write([]byte("Metrics reset to 0"))
+}
+
+
+//validate handler: validates the chirps 
+func validateChirp(w http.ResponseWriter, req *http.Request) {
+	decoder := json.NewDecoder(req.Body)
+	reqChirp := chirp{}
+	err := decoder.Decode(&reqChirp)
+
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte("{\"error\": \"Something went wrong\"}"))
+	} else if len(reqChirp.Body) > 140 {
+		w.WriteHeader(400)
+		w.Write([]byte("{\"error\": \"Chirp is too long\"}"))
+	} else if hasProfanity(reqChirp.Body){
+		cleaned_chirp := cleanProfanity(reqChirp.Body)
+		w.WriteHeader(200)
+		w.Write([]byte(fmt.Sprintf("{\"cleaned_body\": \"%s\"}", cleaned_chirp)))
+	} else {
+		w.WriteHeader(200)
+		w.Write([]byte(fmt.Sprintf("{\"cleaned_body\": \"%s\"}", reqChirp.Body)))
+	}
+}
+
+func hasProfanity(text string) bool {
+	words := strings.Fields(text)
+	for _, word := range words {
+		if strings.ToLower(word) == "kerfuffle" || strings.ToLower(word) == "sharbert" || strings.ToLower(word) == "fornax" {
+			return true
+		}
+	}
+	return false
+}
+
+func cleanProfanity(text string) string {
+	cleaned_chirp := ""
+	words := strings.Fields(text)
+	for _, word := range words {
+		if strings.ToLower(word) == "kerfuffle" || strings.ToLower(word) == "sharbert" || strings.ToLower(word) == "fornax" {
+			cleaned_chirp += "**** "
+		} else {
+			cleaned_chirp += word + " "
+		}
+	}
+	return cleaned_chirp[:len(cleaned_chirp)-1]
 }
